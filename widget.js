46c9225fa546affd34b9eac42811c03fcd66496e
grantlesueur <grantlesueur@gmail.com>
2019-03-19 15:59:51 +0000

    changes w/ c9

--- widget.js
+++ widget.js
@@ -919,7 +919,7 @@ cpdefine("inline:com-chilipeppr-widget-super-touchplate", ["chilipeppr_ready", '
         }
         else {
           
-          var gcode = "G10 L20 P" + this.coordOffsetNo + " Y" + yoffset + "\n";
+          var gcode = "G10 L20 P" + this.coordOffsetNo + " Y" + yoffset +yOffset + "\n";
         }
         var id = "tp" + this.gcodeCtr++;
         chilipeppr.publish("/com-chilipeppr-widget-serialport/jsonSend", {
